421daab603e61508adca750adc0e86fa2ebef71f
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-03-09 16:28:18 -0300

    projeto 17- implementando abstração da classe routes

--- Projeto-17-Mini-Framework/App/Route.php
+++ Projeto-17-Mini-Framework/App/Route.php
@@ -1,22 +1,9 @@
 <?php
 namespace App;
 
-class Route{
+use MF\Init\Bootstrap;
 
-	private $routes;
-
-	public function __construct(){
-		$this->initRoutes();
-		$this->run($this->getUrl());
-	}
-
-	public function getRoutes(){
-		return $this->routes;
-	}
-	public function setRoutes(array $routes){
-		$this->routes = $routes;
-	}
-
+class Route extends Bootstrap{
 
 	public function initRoutes(){
 		$routes['home'] = array(
@@ -33,24 +20,9 @@ class Route{
 		$this->setRoutes($routes);
 	}
 
-	public function run($url){
-		foreach($this->getRoutes() as $key => $route){
-			
-			if($url == $route['route']){
-				$class = "App\\Controllers\\".ucfirst($route['controller']);
 
-				$controller = new $class;
 
-				$action = $route['action'];
-
-				$controller->$action();
-			}
-		}
-	}
-
-	public function getUrl(){
-		return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-	}
+	
 }
 
 

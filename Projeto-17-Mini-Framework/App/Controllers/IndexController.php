c8dd8b69ac6d94680ce131cf2e3bde692b8e1caa
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-03-10 16:57:22 -0300

    projeto 17 - implementando conexção como banco de dados e models

--- Projeto-17-Mini-Framework/App/Controllers/IndexController.php
+++ Projeto-17-Mini-Framework/App/Controllers/IndexController.php
@@ -3,12 +3,27 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class IndexController extends Action {
 
 	public function index(){
 
-		$this->view->dados = array('sofá','cadeira','cama');
+		//$this->view->dados = array('sofá','cadeira','cama');
+
+		// Instanciando conexão
+		$conn = Connection::getDb();
+
+		//Instanciando modelo
+		$produto = new Produto($conn);
+
+		$produtos = $produto->getProdutos();
+
+		$this->view->dados = $produtos;
+
+		//Renderizando conteúdo
+
 		$this->render('index','layout2');
 	}
 

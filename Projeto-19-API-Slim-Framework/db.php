9891f33ab701d2bb009be1a88290c7837f5e7577
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-04-01 14:56:37 -0300

    adicionando rota produtos

--- Projeto-19-API-Slim-Framework/db.php
+++ Projeto-19-API-Slim-Framework/db.php
@@ -30,3 +30,25 @@ $schema->create($table, function($table){
     $table->string('fabricante', 60);
     $table->date('dt_criacao');
 });
+//Populate table products
+$db->table($table)->insert([
+    'titulo' => 'Samatphone Motorola',
+    'descricao' => 'Android Oreo',
+    'preco' => 899.00,
+    'fabricante' => 'Motorola',
+    'dt_criacao' => '2019-10-22'
+]);
+$db->table($table)->insert([
+    'titulo' => 'Iphone',
+    'descricao' => 'IOS 12 4GB',
+    'preco' => 4999.00,
+    'fabricante' => 'Apple',
+    'dt_criacao' => '2020-01-10'
+]);
+$db->table($table)->insert([
+    'titulo' => 'Nokia Tijolão',
+    'descricao' => 'Inquebrável',
+    'preco' => 500.00,
+    'fabricante' => 'Nokia',
+    'dt_criacao' => '2003-01-10'
+]);

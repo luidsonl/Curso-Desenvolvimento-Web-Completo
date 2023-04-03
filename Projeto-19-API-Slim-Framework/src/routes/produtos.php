94b589feac7949acbc836c6419634cf7e1d83159
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-04-03 17:04:39 -0300

    Finalizando rotas

--- Projeto-19-API-Slim-Framework/src/routes/produtos.php
+++ Projeto-19-API-Slim-Framework/src/routes/produtos.php
@@ -5,8 +5,8 @@ use Slim\Http\Response;
 use App\Models\Produto;
 
 // Routes
-// Lista produtos
 $app->group('/api/v1', function(){
+	// Lista produtos
 	$this->get('/produtos/lista', function($request, $response){
 		$produtos = Produto::get();
 		return $response->withJson($produtos);
@@ -16,5 +16,26 @@ $app->group('/api/v1', function(){
 		$dados = $request->getParsedBody();
 		$produto = Produto::create($dados);
 		return $response->withJson($produto);
+	});
+	//Recuperar produto para um determinado id
+	$this->get('/produtos/lista/{id}', function($request, $response, $args){
+		$produto = Produto::findOrFail($args['id']);
+		return $response->withJson($produto);
+		
+	});
+	//Atualiza produto para um determinado id
+	$this->put('/produtos/atualiza/{id}', function($request, $response, $args){
+		$dados = $request->getParsedBody();
+		$produto = Produto::findOrFail($args['id']);
+		$produto->update($dados);
+		return $response->withJson($produto);
+		
+	});
+	//Remove produto para um determinado id
+	$this->get('/produtos/remove/{id}', function($request, $response, $args){
+		$produto = Produto::findOrFail($args['id']);
+		$produto->delete();
+		return $response->withJson($produto);
+		
 	});
 });
